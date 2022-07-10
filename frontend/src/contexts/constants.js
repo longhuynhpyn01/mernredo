fb11c18f74b86f23d4f578b0a4ba233acafdb5cd
Long Huynh <longhuynhpyn01@gmail.com>
2022-07-10 22:33:14 +0700

    upload url

--- frontend/src/contexts/constants.js
+++ frontend/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "https://mernredoapp.herokuapp.com/api"
+    ? "http://localhost:5000/api"
     : "https://mernredoapp.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
